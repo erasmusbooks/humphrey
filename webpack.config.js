22a4a732dd39cda6c80e0154e05f4c6b6a71323a
unknown <ben@ltw703.Erasmus.local>
2016-11-02 22:26:26 +0100

    More build testing

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,6 @@ module.exports = {
 			new webpack.optimize.DedupePlugin(),
 			new webpack.optimize.OccurenceOrderPlugin(),
 			new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-			new webpack.optimize.CommonsChunkPlugin('client.js', Infinity),
+			new webpack.optimize.CommonsChunkPlugin('client.js'),
 	],
 };
